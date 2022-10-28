4e5abcad3ed01c4b3a4e1bf4ee5c9cc54b3c60d3
wdfk-prog <1425075683@qq.com>
2022-10-28 18:16:24 +0800

    [fix][dfs]bug?

--- rt-thread/components/dfs/src/dfs.c
+++ rt-thread/components/dfs/src/dfs.c
@@ -529,7 +529,7 @@ int list_fd(void)
     fd_table = dfs_fdtable_get();
     if (!fd_table) return -1;
 
-    rt_enter_critical();
+//    rt_enter_critical();
 
     rt_kprintf("fd type    ref magic  path\n");
     rt_kprintf("-- ------  --- ----- ------\n");
@@ -562,7 +562,7 @@ int list_fd(void)
             }
         }
     }
-    rt_exit_critical();
+//    rt_exit_critical();
 
     return 0;
 }
