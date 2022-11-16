e19c4eae07b536a85dab6bad4ee3ba6e789f96ae
wdfk-prog <1425075683@qq.com>
2022-11-16 16:14:13 +0800

    [MODBUS]添加电机反馈信息查看

--- BoardDriver/modbus/slave/input_registers.c
+++ BoardDriver/modbus/slave/input_registers.c
@@ -14,6 +14,7 @@
 #include "modbus_slave_common.h"
 /* Private includes ----------------------------------------------------------*/
 #include "master402_canopen.h"
+#include "motor_control.h"
 /* Private typedef -----------------------------------------------------------*/
 
 /* Private define ------------------------------------------------------------*/
@@ -31,16 +32,25 @@ static uint16_t _tab_input_registers[MODBUS_REG_MAX_NUM];
 */
 int modbus_slave_input_register_default(void)
 {
+  //节点参数区域
   _tab_input_registers[1]  = MAX_NODE_COUNT - 1;  //节点数量
-  _tab_input_registers[2]  = 0X0F;                 //节点NMT状态
+  _tab_input_registers[2]  = 0X0F;                //节点NMT状态
   //从03D~06D
   nodeID_get_name((char *)&_tab_input_registers[3],
                    modbus_register_get(0,1));     //节点名称
-  _tab_input_registers[7]  = 0X00;                 //节点错误代码
+  _tab_input_registers[7]  = 0X00;                //节点错误代码
   //08D~10D节点具体错误
   _tab_input_registers[8]  = 0X00;                 //节点具体错误
   _tab_input_registers[9]  = 0X00;                 //节点具体错误
   _tab_input_registers[10] = 0X00;                 //节点具体错误
+  //电机参数区域
+  _tab_input_registers[11] = 0X00;                 //控制指令
+  _tab_input_registers[12] = 0X00;                 //状态位
+  _tab_input_registers[13] = 0X00;                 //当前位置低16位
+  _tab_input_registers[14] = 0X00;                 //当前位置高16位
+  _tab_input_registers[15] = 0X00;                 //当前速度低16位
+  _tab_input_registers[16] = 0X00;                 //当前速度高16位
+  //17D~20D电机保留区域
   return RT_EOK;
 }
 
@@ -52,14 +62,19 @@ int modbus_slave_input_register_default(void)
 */
 void modbus_slave_input_register_write(void)
 {
-  uint8_t nodeID = modbus_register_get(0,1);
-
+  uint8_t nodeID = modbus_register_get(0,1);        
+  //节点参数区域
   _tab_input_registers[2] = nodeID_get_nmt(nodeID);         //节点NMT状态
    //从03D~06D
   nodeID_get_name((char *)&_tab_input_registers[3],nodeID); //节点名称
   _tab_input_registers[7] = nodeID_get_errcode(nodeID);     //节点错误代码
   //08D~10D节点具体错误
   nodeID_get_errSpec((char *)&_tab_input_registers[8],nodeID);
+  //电机参数区域
+  _tab_input_registers[11] = motor_get_controlword(nodeID);         //控制指令
+  _tab_input_registers[12] = motor_get_statusword(nodeID);          //状态位
+  motor_get_position((INTEGER32 *)&_tab_input_registers[13],nodeID);//当前位置
+  motor_get_velocity((INTEGER32 *)&_tab_input_registers[15],nodeID);//当前速度
 }
 /**
   * @brief  获取MODBUS输入寄存器数据
