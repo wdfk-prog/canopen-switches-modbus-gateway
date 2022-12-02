cf5983c481a4b77e266394274a6636ba26788d43
wdfk-prog <1425075683@qq.com>
2022-12-02 10:37:43 +0800

    更新按键驱动

--- board/CubeMX_Config/Inc/stm32f4xx_it.h
+++ board/CubeMX_Config/Inc/stm32f4xx_it.h
@@ -71,6 +71,13 @@ typedef struct
 
 /* Exported functions prototypes ---------------------------------------------*/
 void PVD_IRQHandler(void);
+void EXTI0_IRQHandler(void);
+void EXTI1_IRQHandler(void);
+void EXTI2_IRQHandler(void);
+void EXTI3_IRQHandler(void);
+void EXTI4_IRQHandler(void);
+void EXTI9_5_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
