6cbd356a03a77b4cd246ffcd334bb5fb05637472
wdfk-prog <1425075683@qq.com>
2022-12-26 10:42:35 +0800

    [board][user]adc更新支持测量温度与参考电压

--- BoardDriver/user/user_adc.c
+++ BoardDriver/user/user_adc.c
@@ -1,25 +1,19 @@
-/*
- * 程序清单： ADC 设备使用例程
- * 例程导出了 adc_sample 命令到控制终端
- * 命令调用格式：adc_sample
- * 程序功能：通过 ADC 设备采样电压值并转换为数值。
- *           示例代码参考电压为3.3V,转换位数为12位。
-*/
-
 #include <rtthread.h>
 #include <rtdevice.h>
+#include <main.h>
 
 #define ADC_DEV_NAME        "adc1"      /* ADC 设备名称 */
-#define ADC_DEV_CHANNEL     1           /* ADC 通道 */
-#define REFER_VOLTAGE       330         /* 参考电压 3.3V,数据精度乘以100保留2位小数*/
-#define CONVERT_BITS        (1 << 12)   /* 转换位数为12位 */
+#define ADC_VREF_CHANNEL     ADC_CHANNEL_VREFINT - ADC_CHANNEL_0    /* ADC Vref 通道 */
+#define ADC_TEMP_CHANNEL     ADC_CHANNEL_TEMPSENSOR - ADC_CHANNEL_0 /* ADC Temp 通道 */
 
-static int adc_vol_sample(int argc, char *argv[])
+static int adc_vol_sample(void)
 {
-    rt_adc_device_t adc_dev;
-    rt_uint32_t value, vol;
-    rt_err_t ret = RT_EOK;
+    rt_uint32_t vref_value,temp_value;
+    rt_uint16_t vref_mv,temp_mv; 
 
+    rt_adc_device_t adc_dev;
+    rt_err_t ret = RT_EOK;
+    
     /* 查找设备 */
     adc_dev = (rt_adc_device_t)rt_device_find(ADC_DEV_NAME);
     if (adc_dev == RT_NULL)
@@ -29,20 +23,29 @@ static int adc_vol_sample(int argc, char *argv[])
     }
 
     /* 使能设备 */
-    ret = rt_adc_enable(adc_dev, ADC_DEV_CHANNEL);
-
+    ret = rt_adc_enable(adc_dev, ADC_VREF_CHANNEL);
     /* 读取采样值 */
-    value = rt_adc_read(adc_dev, ADC_DEV_CHANNEL);
-    rt_kprintf("the value is :%d \n", value);
-
-    /* 转换为对应电压值 */
-    vol = value * REFER_VOLTAGE / CONVERT_BITS;
-    rt_kprintf("the voltage is :%d.%02d \n", vol / 100, vol % 100);
-
+    vref_value = rt_adc_read(adc_dev, ADC_VREF_CHANNEL);
     /* 关闭通道 */
-    ret = rt_adc_disable(adc_dev, ADC_DEV_CHANNEL);
+    ret = rt_adc_disable(adc_dev, ADC_VREF_CHANNEL);
 
-    return ret;
+    /* 使能设备 */
+    ret = rt_adc_enable(adc_dev, ADC_TEMP_CHANNEL);
+    /* 读取采样值 */
+    temp_value = rt_adc_read(adc_dev, ADC_TEMP_CHANNEL);
+    /* 关闭通道 */
+    ret = rt_adc_disable(adc_dev, ADC_TEMP_CHANNEL);
+  
+    rt_kprintf("Vref  is %u.\n", vref_value);
+    rt_kprintf("Temp is %u.\n" , temp_value);
+    
+    // Calculating Vref voltage
+    vref_mv = __LL_ADC_CALC_VREFANALOG_VOLTAGE(vref_value, ADC_RESOLUTION_12B);
+    rt_kprintf("Vref voltage is %u mV.\n", vref_mv);
+    // Calculate Temperature
+    rt_kprintf("%d are Temperature in Degree C.\n",
+    __LL_ADC_CALC_TEMPERATURE_TYP_PARAMS(2500,760,10,vref_mv, temp_value, ADC_RESOLUTION_12B));
+    return RT_EOK;
 }
 /* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(adc_vol_sample, adc voltage convert sample);
